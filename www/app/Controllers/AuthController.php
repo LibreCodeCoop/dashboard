00e1b231b82a786b7740eab7a5b3825885d1e704
Samuel Bretas <samuelbretas@gmail.com>
2019-10-03 22:23:36 -0300

    created controller authentication, repository where will group business rule and html for login screen presentation

---
+++ www/app/Controllers/AuthController.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace App\Controllers;
+
+class AuthController extends BaseController
+{
+
+    public function login()
+    {
+        $this->render('login.html');
+    }
+
+    public function connect()
+    {
+
+    }
+
+    public function disconnect()
+    {
+
+    }
+}
