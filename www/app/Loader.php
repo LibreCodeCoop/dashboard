d592a5deeb41d70f321a11589fd6a7e6d9e0291b
Samuel Bretas <samuelbretas@gmail.com>
2019-10-06 16:47:05 -0300

    I installed symfony component for reading dot env file. I made the configuration in the loader file and removed the settings from docker-compose

--- www/app/Loader.php
+++ www/app/Loader.php
@@ -12,28 +12,29 @@ session_start();
 require BASE_PATH . 'vendor/autoload.php';
 require BASE_PATH . 'app/database.php';
 
+//invoke dotenv symfony package
 use Symfony\Component\Dotenv\Dotenv;
-
 $dotenv = new Dotenv();
 $dotenv->load(realpath('/app'). '/.env');
 
 
 /**
- * Instancia o roteador.
+ * invoke router routine
  */
 $app = System\App::instance();
 $app->request = System\Request::instance();
 $app->route = System\Route::instance($app->request);
 $route = $app->route;
 
+
 /**
- * create routes and arrow main route
- */
-/**
- *  route group authentication
+ *  route group
  */
 $route->get(['/', 'index', 'home', 'default'], 'App\Controllers\AuthController@login');
 
+/**
+ * route group authentication
+ */
 $route->group('/auth', function(){
     $this->post('/connect', 'App\Controllers\AuthController@connect');
     $this->get('/disconnect', 'App\Controllers\AuthController@disconnect');
