46d96b3f0acb39433509844b5cd856ce180d2cc8
Raphael de Almeida <jaguarnet7@gmail.com>
2019-11-11 06:29:43 -0300

    final query to call history #4

---
+++ app/Services/CallService.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Services;
+
+use App\Company;
+use Illuminate\Support\Facades\DB;
+
+class CallService
+{
+    public function find()
+    {
+        return DB::select(
+"SELECT CASE WHEN co.id IS NOT NULL THEN co.social_reason ELSE u.name END as cliente,
+       cdr.start_stamp AS start_time,
+       cdr.caller_id_number AS origin_number,
+       cdr.destination_number,
+       CONCAT((cdr.billsec) DIV 60, ':', LPAD((FLOOR(cdr.billsec) MOD 60), 2, 0)) AS 'Duração',
+       gravacoes_s3.path_s3
+FROM
+    " . env('DB_DATABASE_LEGACY') . ".tblcustomfieldsvalues context
+        JOIN ". env('DB_DATABASE_LEGACY'). ".tblcustomfields context_field ON
+                context_field.id = context.fieldid
+            AND context_field.fieldname = 'Dominio no PABX'
+        JOIN ". env('DB_DATABASE_LEGACY') . ".tblhosting d ON
+            d.id = context.relid
+        JOIN ". env('DB_DATABASE_LEGACY') .".tblclients c ON
+            c.id = d.userid
+        JOIN ". env('DB_DATABASE_LEGACY') .".tblcustomfieldsvalues cfv_doc1 ON
+                c.id = cfv_doc1.relid
+            AND cfv_doc1.fieldid = 1
+        JOIN ". env('DB_DATABASE') .".customers cs ON cs.code = d.userid
+        JOIN ". env('DB_DATABASE') .".customer_user cu ON cu.customer_id = cs.id
+        LEFT JOIN ". env('DB_DATABASE') .".companies co ON co.id = cs.typeable_id AND cs.typeable_type = 'App\\Company'
+        LEFT JOIN ". env('DB_DATABASE') .".users u ON u.id = cs.typeable_id AND cs.typeable_type = 'App\\User'
+        JOIN ". env('DB_DATABASE_VOIP') .".cdr cdr ON
+                cdr.context = context.value
+            OR (cdr.context = SUBSTRING_INDEX(context.value, '/', 1)
+            AND (cdr.caller_id_number = SUBSTRING_INDEX(context.value, '/', - 1)
+                OR cdr.destination_number = SUBSTRING_INDEX(context.value, '/', - 1)))
+        LEFT JOIN ". env('DB_DATABASE_VOIP') .".gravacoes_s3 ON
+            cdr.uuid = gravacoes_s3.uuid
+-- WHERE
+--        u.id = :id -- Quando não for admin, vem do id da sessão, este é o id do usuário logado
+--   AND (cs.typeable_id = :typeable_id AND cs.typeable_type = :typeable_type) -- Filtro pelo customer do usuário logado
+  -- AND cdr.start_stamp = :start_samp -- Data inicial
+  -- AND cdr.caller_id_number = :caller_id_number -- número de origem
+  -- AND cdr.destination_number = :destination_number ? -- Número de destino
+ORDER BY
+    start_time DESC;
+"
+,
+            [
+                'id' => 1,
+                'typeable_id' => 1,
+                'typeable_type' => 'App\\User',
+            ]
+        );
+    }
+
+}
