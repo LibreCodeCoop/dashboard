994e4e95ce71e5907524791db55dfc9943bf967d
Raphael de Almeida <jaguarnet7@gmail.com>
2019-12-05 06:17:25 -0300

    #3 FIX init datatables api token

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -103,98 +103,100 @@
 
             return "<span class='badge badge-" + color + "'>" + status + "</span>";
         }
+        $(document).ready(function() {
 
-        var table = $('#invoices-table').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: '{{ route('api_invoice.index') }}',
-            orderCellsTop: true,
-            fixedHeader: true,
-            columns: [
-                {data: 'invoice_code', name: 'invoice_code'},
-                {data: 'client', name: 'client'},
-                {data: 'date', name: 'date'},
-                {data: 'duedate', name: 'duedate'},
-                {data: 'total', name: 'total'},
-                {data: 'status', name: 'status'},
-                {data: 'action', name: 'action', orderable: false, searchable: false}
-            ],
-            "columnDefs": [
-                {
-                    "render": function ( data, type, row ) {
-                        return filterStatus(data);
+            var table = $('#invoices-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: '{{ route('api_invoice.index') }}',
+                orderCellsTop: true,
+                fixedHeader: true,
+                columns: [
+                    {data: 'invoice_code', name: 'invoice_code'},
+                    {data: 'client', name: 'client'},
+                    {data: 'date', name: 'date'},
+                    {data: 'duedate', name: 'duedate'},
+                    {data: 'total', name: 'total'},
+                    {data: 'status', name: 'status'},
+                    {data: 'action', name: 'action', orderable: false, searchable: false}
+                ],
+                "columnDefs": [
+                    {
+                        "render": function (data, type, row) {
+                            return filterStatus(data);
+                        },
+                        "targets": 5
                     },
-                    "targets": 5
-                },
-            ],
-            initComplete: function () {
-                $('#invoices-table thead tr').clone().appendTo( '#invoices-table thead' );
-                this.api().columns().every(function (index) {
-                    var column = this;
+                ],
+                initComplete: function () {
+                    $('#invoices-table thead tr').clone().appendTo('#invoices-table thead');
+                    this.api().columns().every(function (index) {
+                        var column = this;
 
-                    if(index === 6) {
-                        $(document.createElement("span")).appendTo($(column.header()).empty())
-                        return;
-                    }else if (index === 5 ) {
-                        var select = $(document.createElement("select"));
-                        select.addClass("custom-select custom-select-sm form-control form-control-sm");
-                        ['', 'Em atraso', 'Em aberto', 'Pago', 'Cancelada'].forEach(function (e) {
-                            select.append($('<option>', {
-                                value: e,
-                                text: e
-                            }))
-                        })
-                        select.appendTo($(column.header()).empty())
-                        select.on('change', function () {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
-
-                            column
-                                .search(val ? '^' + val + '$' : '', true, false)
-                                .draw();
-                        });
-
-                    } else if(index === 1) {
-
-                        var select = $(document.createElement("select"));
-                        select.addClass("custom-select custom-select-sm form-control form-control-sm");
-                        select.append($('<option>'))
-                        $.get('{{ route("api_customer.index") }}', function (data) {
-                            data.forEach(function (e) {
-                                select.append($('<option>',{
-                                    // value: e.id,
-                                    text: (e.typeable_type == 'App\\User')? e.typeable.name : e.typeable.social_reason,
-                                    value: (e.typeable_type == 'App\\User')? e.typeable.name : e.typeable.social_reason
-
+                        if (index === 6) {
+                            $(document.createElement("span")).appendTo($(column.header()).empty())
+                            return;
+                        } else if (index === 5) {
+                            var select = $(document.createElement("select"));
+                            select.addClass("custom-select custom-select-sm form-control form-control-sm");
+                            ['', 'Em atraso', 'Em aberto', 'Pago', 'Cancelada'].forEach(function (e) {
+                                select.append($('<option>', {
+                                    value: e,
+                                    text: e
                                 }))
                             })
-                        })
-                        select.appendTo($(column.header()).empty())
-                        select.on('change', function () {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).val()
-                            );
+                            select.appendTo($(column.header()).empty())
+                            select.on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                    $(this).val()
+                                );
 
-                            column
-                                .search(val ? '^' + val + '$' : '', true, false)
-                                .draw();
-                        });
-
-                     }else {
-
-                        var input = document.createElement("input");
-                        $(input).appendTo($(column.header()).empty())
-                            .on('keyup change clear', function () {
-                                if (column.search() !== this.value) {
-                                    column
-                                        .search(this.value)
-                                        .draw();
-                                }
+                                column
+                                    .search(val ? '^' + val + '$' : '', true, false)
+                                    .draw();
                             });
-                    }
-                });
-            }
+
+                        } else if (index === 1) {
+
+                            var select = $(document.createElement("select"));
+                            select.addClass("custom-select custom-select-sm form-control form-control-sm");
+                            select.append($('<option>'))
+                            $.get('{{ route("api_customer.index") }}', function (data) {
+                                data.forEach(function (e) {
+                                    select.append($('<option>', {
+                                        // value: e.id,
+                                        text: (e.typeable_type == 'App\\User') ? e.typeable.name : e.typeable.social_reason,
+                                        value: (e.typeable_type == 'App\\User') ? e.typeable.name : e.typeable.social_reason
+
+                                    }))
+                                })
+                            })
+                            select.appendTo($(column.header()).empty())
+                            select.on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                    $(this).val()
+                                );
+
+                                column
+                                    .search(val ? '^' + val + '$' : '', true, false)
+                                    .draw();
+                            });
+
+                        } else {
+
+                            var input = document.createElement("input");
+                            $(input).appendTo($(column.header()).empty())
+                                .on('keyup change clear', function () {
+                                    if (column.search() !== this.value) {
+                                        column
+                                            .search(this.value)
+                                            .draw();
+                                    }
+                                });
+                        }
+                    });
+                }
+            });
         });
     </script>
     <style>
