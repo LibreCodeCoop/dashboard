be948a28867765a3c78de9d0165778dc281393bc
Raphael de Almeida <jaguarnet7@gmail.com>
2019-12-09 23:28:16 -0300

    #1 layout custom colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,13 @@
     <link href="{{ asset('material') }}/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link href="{{ asset('material') }}/demo/demo.css" rel="stylesheet" />
+    <style>
+        :root {
+            --app-background-color: {{ env('APP_BACKGROUND_COLOR', '#f15a22') }};
+            --app-background-color-light: {{ env('APP_BACKGROUND_COLOR_LIGHT', '#f17959') }};
+        }
+    </style>
+    <link href="{{ asset('css') }}/app.css" rel="stylesheet" />
     </head>
     <body class="{{ $class ?? '' }}">
         @auth()
