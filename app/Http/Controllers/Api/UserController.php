452accc44ecd52236c30d5ea888c594c003b4d87
Vitor Mattos <vitor@php.rio>
2020-01-07 20:11:16 -0300

    Bugfix: block exclude user when user is primary user of customer

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\DataTables;
 
 class UserController
@@ -13,7 +14,17 @@ class UserController
         $user = Auth::guard('api')->user();
         $user = (!$user->is_admin)? $user : null;
 
-        $query = $dataTables->eloquent(User::query());
+        $query = User::query();
+        $query->select('users.id', 'users.name', 'users.email', 'users.created_at',
+                DB::raw('CASE WHEN customers.id IS NOT NULL THEN 1 ELSE 0 END as `primary`')
+            )
+            ->leftJoin(DB::raw('customers'), function($join) {
+                $join->on('customers.typeable_id', '=', 'users.id');
+                $join->on('customers.typeable_type', '=', DB::raw("'App\\\\User'"));
+            });
+        $row = $query->get();
+
+        $query = $dataTables->eloquent($query);
         $query->addIndexColumn()
             ->addColumn('customers', function ($user) {
                 return $user->customers->map(function ($c) {
@@ -23,20 +34,24 @@ class UserController
                 })->implode(' | ');
             })
             ->addColumn('action', function ($user) {
-                return
-                "<form action='" . route('user.destroy', $user) . "' method='post' class='user-form'>
-                      " . csrf_field() . "
-                      " . method_field('delete') . "
+                $html = "<form action='" . route('user.destroy', $user) . "' method='post' class='user-form'>";
+                $html.= csrf_field();
+                $html.= method_field('delete');
+                $html.= "
                       <a rel='tooltip' class='btn btn-success btn-link' href='" . route('user.edit', $user) ."' data-original-title='' title=''>
                         <i class='material-icons'>edit</i>
                         <div class='ripple-container'></div>
-                      </a>
-                      <button type='button' class='btn btn-danger btn-link' data-original-title='' title=''
-                          onclick=\"confirm('" . __("Are you sure you want to delete this user?") . " ') ? submitExcludeUser(this.parentElement) : ''\">
-                          <i class='material-icons'>close</i>
-                          <div class='ripple-container'></div>
-                      </button>
-                 </form>";
+                      </a>";
+                if (!$user->primary) {
+                    $html.= "
+                        <button type='button' class='btn btn-danger btn-link' data-original-title='' title=''
+                            onclick=\"confirm('" . __("Are you sure you want to delete this user?") . " ') ? submitExcludeUser(this.parentElement) : ''\">
+                            <i class='material-icons'>close</i>
+                            <div class='ripple-container'></div>
+                        </button>";
+                }
+                $html.= "</form>";
+                return $html;
             });
 
         return $query->make(true);
