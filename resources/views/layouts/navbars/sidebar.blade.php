7ff84061017435af0838738055cc3a875597afd8
Daiane <you@example.com>
2020-01-08 11:32:16 -0300

    Alteração de ícones da página inicial

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -9,26 +9,26 @@
         @can('isAdmin')
         <li class="nav-item{{ $activePage == 'customer-management' ? ' active' : '' }}">
             <a class="nav-link" href="{{ route('customer.index') }}">
-                <i class="material-icons">content_paste</i>
+                <i class="material-icons ">supervisor_account</i>
                 <p>{{ __('Customer Management') }}</p>
             </a>
         </li>
         <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
             <a class="nav-link" href="{{ route('user.index') }}">
-                <i class="material-icons">content_paste</i>
+                <i class="material-icons">perm_identity</i>
                 <span class="sidebar-normal"> {{ __('User Management') }} </span>
             </a>
         </li>
         @endcan
         <li class="nav-item{{ $activePage == 'call-history' ? ' active' : '' }}">
             <a class="nav-link" href="{{ route('call.index') }}">
-                <i class="material-icons">content_paste</i>
+                <i class="material-icons">phonelink_ring</i>
                 <span class="sidebar-normal"> {{ __('Call History') }} </span>
             </a>
         </li>
         <li class="nav-item{{ $activePage == 'invoice-listing' ? ' active' : '' }}">
             <a class="nav-link" href="{{ route('invoice.index') }}">
-                <i class="material-icons">content_paste</i>
+                <i class="material-icons">attach_money</i>
                 <span class="sidebar-normal"> {{ __('Invoice Listing') }} </span>
             </a>
         </li>
