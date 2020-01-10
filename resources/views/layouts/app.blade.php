c6b56358216387c9d8f938cd551c89e36c3bdcd7
Vitor Mattos <vitor@php.rio>
2020-01-09 21:54:13 -0300

    Remove demo files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
     <link href="{{ asset('material') }}/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{ asset('material') }}/demo/demo.css" rel="stylesheet" />
     <style>
         :root {
             --app-background-color: {{ env('APP_BACKGROUND_COLOR', '#f15a22') }};
--- resources/views/layouts/invoice.blade.php
+++ resources/views/layouts/invoice.blade.php
@@ -14,8 +14,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
     <link href="{{ asset('material') }}/css/material-dashboard.css?v=2.1.1" rel="stylesheet" />
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="{{ asset('material') }}/demo/demo.css" rel="stylesheet" />
 </head>
 <body class="{{ $class ?? '' }}">
 @yield('content')
