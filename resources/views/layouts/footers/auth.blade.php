136d148dba0a3b6338dfc241a937629e9ca55d16
Vitor Mattos <vitor@php.rio>
2021-08-24 16:05:38 -0300

    Update auth.blade.php

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -5,7 +5,7 @@
       <script>
         document.write(new Date().getFullYear())
       </script>, made with <i class="material-icons">favorite</i> by
-      <a href="http://www.lt.coop.br" target="_blank">Lyseon Tech</a> for a better web.
+      <a href="http://www.lt.coop.br" target="_blank">LibreCode</a> for a better web.
     </div>
   </div>
 </footer>
